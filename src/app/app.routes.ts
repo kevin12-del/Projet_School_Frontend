28f97e7bfc57ffa5466af64aa3500afb1d814203
kevin12-del <duramelkenfack@gmail.com>
2025-05-07 00:46:01 +0200

    begin the student page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,26 @@ import {EleveDashComponent} from "./features/eleves/eleve-dash/eleve-dash.compon
 import {EleveProfilComponent} from "./features/eleves/eleve-profil/eleve-profil.component";
 import {ForgetPasswordComponent} from "./features/auths/forget-password/forget-password.component";
 import {EleveNoteComponent} from "./features/eleves/eleve-note/eleve-note.component";
+import {DashbordComponent} from "./features/eleves/dashbord/dashbord.component";
+import {ScheduleComponent} from "./features/eleves/schedule/schedule.component";
+import {CourseComponent} from "./features/eleves/course/course.component";
+import {ClassComponent} from "./features/eleves/class/class.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
   {path: "verif-email", component: VerifEmailComponent},
-  {path: "eleve", component: EleveDashComponent},
-  {path:  "eleve-profil", component: EleveProfilComponent},
+  {path: "eleve", component: EleveDashComponent, children: [
+      {path:  "profil", component: EleveProfilComponent},
+      {path: "grades", component: EleveNoteComponent},
+      {path: "dashbord", component: DashbordComponent},
+      {path: "schedule", component: ScheduleComponent},
+      {path: "courses", component: CourseComponent},
+      {path: "class", component: ClassComponent},
+      {path: "", redirectTo: "dashbord", pathMatch: "full"}
+    ]},
+
   {path: "forget-password", component: ForgetPasswordComponent},
-  {path: "eleve-note", component: EleveNoteComponent}
+
 ];
--- src/app/features/eleves/eleve-dash/eleve-dash.component.ts
+++ src/app/features/eleves/eleve-dash/eleve-dash.component.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {AuthService} from "../../../services/auth.service";
@@ -9,7 +9,7 @@ import {AuthService} from "../../../services/auth.service";
 @Component({
   selector: 'app-eleve-dash',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './eleve-dash.component.html',
   styleUrl: './eleve-dash.component.css'
 })
@@ -21,5 +21,35 @@ export class EleveDashComponent {
 
   logout() {
     this.authService.logout();
+  }
+
+  // Données simulées de l'utilisateur
+  userData = {
+    userName: 'Jean Dupont',
+    userRole: 'Student'
+  };
+  isSidebarCollapsed = false;
+
+
+  // Liste des items du sidebar
+  sidebarItems = [
+    { id: 'dashbord', name: 'Dashbord', icon: 'bi-speedometer2', path: '/eleve/dashbord' },
+    { id: 'profile', name: 'Profil', icon: 'bi-person', path: '/eleve/profil' },
+    { id: 'class', name: 'class', icon: 'bi-people', path: '/eleve/class' },
+    { id: 'schedule', name: 'schedule', icon: 'bi-calendar3', path: '/eleve/schedule' },
+    { id: 'courses', name: 'Courses', icon: 'bi-book', path: '/eleve/courses' },
+    { id: 'grades', name: 'grades', icon: 'bi-star', path: '/eleve/grades' }
+  ];
+
+  isDropdownOpen = false;
+
+  // Fonction pour basculer entre la sidebar étendue et réduite
+  toggleSidebar() {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
+
+  // Fonction pour ouvrir/fermer le menu déroulant de l'utilisateur
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 }
